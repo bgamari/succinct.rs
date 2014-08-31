a30830bceb5b5b19b94cda770e79c1201e35a220
Ben Gamari <bgamari.foss@gmail.com>
2014-08-31 18:40:14 -0400

    rank9: Renamings and begin implementing select

--- succinct/rank9.rs
+++ succinct/rank9.rs
@@ -1,12 +1,33 @@
 use super::num::integer::Integer;
 use std::num::{zero, one, Int};
-use super::dictionary::BitRank;
+use super::dictionary::{BitRank, BitSelect};
+use std::slice::{Found, NotFound};
 
 struct Counts {
     /// first level count (rank up to p)
-    first: u64,
+    block_rank: u64,
     /// second level counts (rank up to each broadword)
-    second: u64,
+    word_ranks: u64,
+}
+
+impl Counts {
+    /// The rank within the block up to the `i`th broadword
+    fn word_rank(&self, i: uint) -> uint {
+        debug_assert!(i < 8);
+        ((self.word_ranks >> (9*i)) & 0x1ff) as uint
+    }
+
+    /// Search for the word that contains the `n`th one within the
+    /// block
+    fn search_word(&self, n: uint) -> uint {
+        debug_assert!(self.word_rank(0) <= n);
+        for i in range(0,7) {
+            if self.word_rank(i) > n {
+                return i-1;
+            }
+        }
+        return 7;
+    }
 }
 
 /// Bitvector supporting efficient rank and select
@@ -38,7 +59,7 @@ impl Rank9 {
 
         // Compute counts
         let mut counts: Vec<Counts> = Vec::with_capacity(n_blocks as uint);
-        let mut accum = Counts { first: 0, second: 0 };
+        let mut accum = Counts { block_rank: 0, word_ranks: 0 };
         // accumulate number of ones in this block
         let mut block_accum: u64 = 0;
         // accumulate number of ones
@@ -52,11 +73,11 @@ impl Rank9 {
                 // push new block
                 counts.push(accum);
                 block_accum = 0;
-                accum.first = rank_accum;
-                accum.second = 0;
+                accum.block_rank = rank_accum;
+                accum.word_ranks = 0;
             } else {
-                accum.second >>= 9;
-                accum.second |= block_accum << (9*6);
+                accum.word_ranks >>= 9;
+                accum.word_ranks |= block_accum << (9*6);
             }
         }
 
@@ -77,15 +98,40 @@ impl BitRank for Rank9 {
 
         // compute second-level contribution
         let shift = (t + ((t >> 60) & 8)) * 9;
-        let second = (counts.second >> (shift as uint)) & 0x1ff;
+        let word_rank = (counts.word_ranks >> (shift as uint)) & 0x1ff;
 
         // within-word contribution
         let masked = self.buffer[word as uint] & ((1 << (bit as uint)) - 1);
 
-        (counts.first + second + masked.count_ones()) as int
+        (counts.block_rank + word_rank + masked.count_ones()) as int
     }
     fn rank0(&self, n: int) -> int {
         0
+    }
+}
+
+impl BitSelect for Rank9 {
+    fn select(&self, bit: bool, n: int) -> int {
+        // uses `laura-select`
+        debug_assert!(n >= 0);
+        let res = self.counts.as_slice().binary_search(|x| x.block_rank.cmp(&(n as u64)));
+        match res {
+            Found(i) => {
+                self.buffer[i*8].select(bit, 0)
+            },
+            NotFound(i) => {
+                let block = i - 1;
+                let counts = &self.counts[block];
+                        for j in range(0,7) {
+                            println!("{} = {}", j, counts.word_rank(j));
+                        }
+                let word_idx = counts.search_word(n as uint - counts.block_rank as uint);
+
+                let mut word = self.buffer[word_idx];
+                let remain: int = n - counts.block_rank as int - counts.word_rank(word_idx) as int;
+                (block as int)*64*8 + (word_idx as int)*8 + word.select(bit, remain) as int
+            }
+        }
     }
 }
 
@@ -122,6 +168,26 @@ mod test {
             let a = bv.rank1(select);
             if a != rank {
                 fail!("rank1({}) failed: expected {}, saw {}", select, rank, a);
+            }
+        }
+    }
+
+    #[test]
+    fn test_select1() {
+        let v = vec!(0b0110, 0b1001, 0b1100);
+        let bv = Rank9::from_vec(v, 64*3);
+        let select1: Vec<(int,int)> = vec!(
+            (0, (1+0*64)), // rank is non exclusive rank of zero is always 0
+            (1, (2+0*64)),
+            (2, (0+1*64)),
+            (3, (3+1*64)),
+            (4, (2+2*64)),
+            (5, (3+2*64)),
+        );
+        for &(rank, select) in select1.iter() {
+            let a = bv.select1(rank);
+            if a != select {
+                fail!("select1({}) failed: expected {}, saw {}", rank, select, a);
             }
         }
     }
