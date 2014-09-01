599945129d847baf01422f6e768d00ed8aa9c09a
Ben Gamari <bgamari.foss@gmail.com>
2014-09-01 18:34:18 -0400

    rank9: select almost works

--- src/rank9.rs
+++ src/rank9.rs
@@ -7,35 +7,46 @@ use std::num::{zero, One, one, Int};
 use super::dictionary::{BitRank, Select, Access};
 use std::collections::Collection;
 
+use std::fmt::Show;
+
 /// Counts for a basic block
 struct Counts {
     /// first level count (rank up to p)
-    block_rank: u64,
+    _block_rank: u64,
     /// second level counts (rank up to each broadword)
     word_ranks: u64,
 }
 
 impl Counts {
-    /// The rank within the block up to (and inclusive of) the `i`th broadword
+    /// The rank within the block up to but not including the `i`th broadword
     fn word_rank(&self, bit:bool, i: uint) -> uint {
         debug_assert!(i < 8);
-        let ones = ((self.word_ranks >> (9*i)) & 0x1ff) as uint;
-        if bit {
-            ones
-        } else {
-            (i+1)*64 - ones
+        match i {
+            0 => 0,
+            _ => {
+                let ones = ((self.word_ranks >> (9*(i-1))) & 0x1ff) as uint;
+                if bit {
+                    ones
+                } else {
+                    i*64 - ones
+                }
+            }
         }
     }
 
-    fn block_rank0(&self, block_idx: uint) -> u64 {
-        64*8*(block_idx as u64) - self.block_rank
+    /// The number of matching bits in blocks up to but not including `block_idx`
+    fn block_rank(&self, bit: bool, block_idx: uint) -> u64 {
+        match bit {
+            true => self._block_rank,
+            false => 64*8*(block_idx as u64) - self._block_rank,
+        }
     }
 
-    /// Search for the word that contains the `n`th one within this
-    /// block
-    fn select_word(&self, n: uint, bit:bool) -> uint {
-        for i in range(0,7) {
-            if self.word_rank(bit, i) > n {
+    /// Search for the word that contains the `n`th matching bit
+    /// within this block
+    fn select_word(&self, bit: bool, n: uint) -> uint {
+        for i in range(0,6) {
+            if n < self.word_rank(bit, i + 1) {
                 return i;
             }
         }
@@ -75,9 +86,31 @@ fn div_ceil<T: Integer>(a: T, b: T) -> T {
 }
 
 impl Rank9 {
+    /// Search for the block that contains the `n`th matching bit
+    fn select_block(&self, bit: bool, n: uint) -> uint {
+        let block_search =
+            binary_search(|idx| self.counts[*idx].block_rank(bit, *idx).cmp(&(n as u64)),
+                          0, self.counts.len());
+        let start_block = match block_search {
+            Found(block) => block,
+            NotFound(i) => return i - 1,
+        };
+
+        // we found a block that is potentially surrounded by blocks
+        // with the block rank; we need to find the next matching
+        // bit
+        for block_idx in range(start_block, self.counts.len()) {
+            if self.counts[block_idx as uint].block_rank(bit, block_idx) != n as u64 {
+                return block_idx - 1;
+            }
+        }
+        self.counts.len() - 1
+    }
+
     pub fn from_vec<'a>(v: &'a Vec<u64>, length_in_bits: int) -> Rank9 {
         let n_blocks = div_ceil(length_in_bits, 64*8);
         let mut v = v.clone(); // FIXME
+        assert!(length_in_bits > 0);
 
         // add padding to end as necessary
         if length_in_bits % (64*8) != 0 {
@@ -87,7 +120,7 @@ impl Rank9 {
 
         // compute counts
         let mut counts: Vec<Counts> = Vec::with_capacity(n_blocks as uint);
-        let mut accum = Counts { block_rank: 0, word_ranks: 0 };
+        let mut accum = Counts { _block_rank: 0, word_ranks: 0 };
         // accumulate number of ones in this block
         let mut block_accum: u64 = 0;
         // accumulate number of ones
@@ -100,7 +133,7 @@ impl Rank9 {
                 // push new block
                 counts.push(accum);
                 block_accum = 0;
-                accum.block_rank = rank_accum;
+                accum._block_rank = rank_accum;
                 accum.word_ranks = 0;
             } else {
                 accum.word_ranks >>= 9;
@@ -139,13 +172,13 @@ impl BitRank for Rank9 {
         // TODO: kill me
         match block_word {
             0 => {},
-            _ => assert_eq!(counts.word_rank(true, block_word as uint - 1), word_rank as uint),
+            _ => assert_eq!(counts.word_rank(true, block_word as uint), word_rank as uint),
         };
 
         // within-word contribution
         let masked = self.buffer[word as uint] & ((1 << (bit_idx as uint)) - 1);
 
-        (counts.block_rank + word_rank + masked.count_ones()) as int
+        (counts._block_rank + word_rank + masked.count_ones()) as int
     }
 
     fn rank0(&self, n: int) -> int {
@@ -153,12 +186,15 @@ impl BitRank for Rank9 {
     }
 }
 
+#[deriving(Show, PartialEq, Eq)]
 enum BinarySearchResult<T> {
     Found(T),
     NotFound(T),
 }
 
-fn binary_search_with_idx<T: Num + Shr<uint,T> + Ord + One + Clone>(cmp: |&T| -> Ordering, lower: T, upper: T) -> BinarySearchResult<T> {
+fn binary_search<T: Num + Shr<uint,T> + Ord + One + Clone + Show>(
+        cmp: |&T| -> Ordering, lower: T, upper: T)
+        -> BinarySearchResult<T> {
     let mut base : T = lower.clone();
     let mut lim : T = upper.clone();
 
@@ -179,40 +215,17 @@ fn binary_search_with_idx<T: Num + Shr<uint,T> + Ord + One + Clone>(cmp: |&T| ->
 
 impl Select<bool> for Rank9 {
     fn select(&self, bit: &bool, n: int) -> int {
-        println!("select{}({})", bit, n)
         let bit = *bit;
         // uses `laura-select`
         debug_assert!(n >= 0);
-        let block_search = if bit {
-                binary_search_with_idx(|idx| self.counts[*idx].block_rank.cmp(&(n as u64)), 0, self.counts.len())
-                //self.counts.as_slice().binary_search(|x| x.block_rank.cmp(&(n as u64)))
-            } else {
-                binary_search_with_idx(|idx| self.counts[*idx].block_rank0(*idx).cmp(&(n as u64)), 0, self.counts.len())
-            };
-        match block_search {
-            Found(block) => {
-                // We found a block beginning with exactly the right rank; We're done.
-                println!("found block {}", block);
-                (block as int)*64*8 + self.buffer[block*8].select(&bit, 0)
-            },
-            NotFound(i) => {
-                // We found the block succceeding the block containing
-                // the desired rank.
-                let block = i - 1;
-                let counts = &self.counts[block];
-                assert!(n > counts.block_rank as int);
-                let word_idx = counts.select_word(n as uint - counts.block_rank as uint, bit);
 
-                let word: u64 = self.buffer[word_idx];
-                let remain: int = if word_idx > 0 {
-                    n - counts.block_rank as int - counts.word_rank(bit, word_idx - 1) as int
-                } else {
-                    n - counts.block_rank as int
-                };
-                println!("NotFound({}) remain={} block:{}/{} word:{}/{} bit:{}  bitvector:{:t}", i, remain, block, (block as int)*64*8, word_idx, (word_idx as int)*64, word.select(&bit, remain) as int, word);
-                (block as int)*64*8 + (word_idx as int)*64 + word.select(&bit, remain) as int
-            }
-        }
+        let block_idx = self.select_block(bit, n as uint);
+        let counts = &self.counts[block_idx];
+        let mut remaining = n - counts.block_rank(bit, block_idx) as int;
+        let word_idx = counts.select_word(bit, remaining as uint);
+        let word: u64 = self.buffer[word_idx];
+        remaining -= counts.word_rank(bit, word_idx) as int;
+        (block_idx as int)*64*8 + (word_idx as int) * 64 + word.select(&bit, remaining)
     }
 }
 
@@ -267,5 +280,29 @@ mod test {
             Some(ans) =>
                 TestResult::from_bool(ans == bv.select(&bit, n as int))
         }
+    }
+
+    #[test]
+    fn test_binary_search() {
+        use super::{binary_search, Found, NotFound};
+        let xs: Vec<int> = vec!(0, 3, 5, 8);
+        assert_eq!(binary_search(|i| xs[*i].cmp(&5), 0, xs.len()), Found(2));
+        assert_eq!(binary_search(|i| xs[*i].cmp(&4), 0, xs.len()), NotFound(2));
+        assert_eq!(binary_search(|i| xs[*i].cmp(&0), 0, xs.len()), Found(0));
+        assert_eq!(binary_search(|i| xs[*i].cmp(&9), 0, xs.len()), NotFound(4));
+    }
+
+    #[quickcheck]
+    fn binary_search_works(v: Vec<int>, s: int) -> TestResult {
+        use std::iter::FromIterator;
+        use super::{binary_search, Found, NotFound};
+        if v.len() < 2 {return TestResult::discard()}
+        let xs: Vec<int> = FromIterator::from_iter(
+            v.move_iter().scan(0, |acc, x| {*acc += x; Some(*acc+x)}));
+        let res = match binary_search(|i| xs[*i].cmp(&s), 0, xs.len()) {
+            Found(i) => xs[i] == s,
+            NotFound(i) => xs[i-1] < s && xs[i] > s
+        };
+        TestResult::from_bool(res)
     }
 }
