175e0b052d65c6c78362fa33b456bf13273a15cc
Ben Gamari <bgamari.foss@gmail.com>
2014-08-31 20:22:09 -0400

    dictionary: Kill PopCount

--- succinct/dictionary.rs
+++ succinct/dictionary.rs
@@ -2,36 +2,17 @@
 ///
 /// Bit indices start at 0.
 
-#[cfg(use_intrinsics)]
-use std::intrinsics::{ctpop64};
-
-/// Count the number of ones in a value
-#[deprecated]
-pub trait PopCount {
-    fn pop_count(&self) -> int;
-}
-
-impl PopCount for u64 {
-    #[inline(always)]
-    #[cfg(not(use_intrinsics))]
-    fn pop_count(&self) -> int {
-        // Broadword sideways addition
-        let x: u64 = *self;
-        let x0: u64 = x - ((x & 0xaaaa_aaaa_aaaa_aaaa) >> 1);
-        let x1: u64 = (x0 & 0x3333_3333_3333_3333) + ((x0 >> 2) & 0x3333_3333_3333_3333);
-        let x2: u64 = (x1 + (x1 >> 4)) & 0x0F0F0_F0F0_F0F0_F0F;
-        let l8: u64 = 0x0101_0101_0101_0101;
-        ((x2 * l8) >> 56) as int
-    }
-
-    #[inline(always)]
-    #[cfg(use_intrinsics)]
-    fn pop_count(&self) -> int {
-        unsafe { ctpop64(*self) as int }
-    }
+fn pop_count(x: u64) -> int {
+    // Broadword sideways addition
+    let x0: u64 = x - ((x & 0xaaaa_aaaa_aaaa_aaaa) >> 1);
+    let x1: u64 = (x0 & 0x3333_3333_3333_3333) + ((x0 >> 2) & 0x3333_3333_3333_3333);
+    let x2: u64 = (x1 + (x1 >> 4)) & 0x0F0F0_F0F0_F0F0_F0F;
+    let l8: u64 = 0x0101_0101_0101_0101;
+    ((x2 * l8) >> 56) as int
 }
 
 pub trait BitAccess {
+    /// Retrieve the `n`th bit
     fn get(&self, n: Pos) -> bool;
 }
 
@@ -121,14 +102,7 @@ impl BitRank for u64 {
 
 #[cfg(test)]
 mod test {
-    use super::PopCount;
     use super::BitSelect;
-
-    #[test]
-    pub fn test_pop_count() {
-        assert_eq!(0x1u64.pop_count(), 1);
-        assert_eq!(0xffu64.pop_count(), 8);
-    }
 
     #[test]
     pub fn test_u64_select() {
