c00106c148cd3992f9e22b62d0c10c853c05a506
Ben Gamari <bgamari.foss@gmail.com>
2014-08-31 21:48:45 -0400

    naive: select implementation

--- src/naive.rs
+++ src/naive.rs
@@ -1,4 +1,5 @@
 use super::dictionary::{BitAccess};
+use std::collections::Collection;
 
 /// A very simple rank implementation written to test against
 pub fn rank<T: BitAccess>(v: &T, bit: bool, n: int) -> int {
@@ -9,4 +10,17 @@ pub fn rank<T: BitAccess>(v: &T, bit: bool, n: int) -> int {
         }
     }
     accum
+}
+
+pub fn select<T: BitAccess + Collection>(v: &T, bit: bool, n: int) -> int {
+    let mut n = n;
+    for i in range(0, v.len()) {
+        if v.get(i as int) == bit {
+            if n == 0 {
+                return i as int;
+            }
+            n -= 1;
+        }
+    }
+    fail!("There aren't that many");
 }
